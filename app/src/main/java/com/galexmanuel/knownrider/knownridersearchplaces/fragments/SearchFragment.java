0d899e08544e4f70b68fde2c000d0fef83fe1409
knownrider <knownrider@gmail.com>
2015-08-24 14:38:40 -0700

    add some text and my name

--- app/src/main/java/com/galexmanuel/knownrider/knownridersearchplaces/fragments/SearchFragment.java
+++ app/src/main/java/com/galexmanuel/knownrider/knownridersearchplaces/fragments/SearchFragment.java
@@ -5,8 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.galexmanuel.knownrider.knownridersearchplaces.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,6 +18,15 @@ import com.galexmanuel.knownrider.knownridersearchplaces.R;
 public class SearchFragment extends Fragment {
 
     private static final String TAG = SearchFragment.class.getSimpleName();
+
+    @Bind(R.id.tvName)
+    protected TextView mTvName;
+
+    @Bind(R.id.tvDescription)
+    protected TextView mTvDescription;
+
+    @Bind(R.id.tvEmail)
+    protected TextView mTvEmail;
 
     public static SearchFragment newInstance() {
         SearchFragment fragment = new SearchFragment();
@@ -28,12 +41,17 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        ButterKnife.bind(this, rootView);
+        mTvName.setText(getString(R.string.my_name));
+        mTvDescription.setText(getString(R.string.my_description));
+        mTvEmail.setText(getString(R.string.my_email));
 
         return rootView;
     }
 
     @Override
     public void onDestroyView() {
+        ButterKnife.unbind(this);
         super.onDestroyView();
     }
 }
